194f658634ea174dff5c5c68ae0dee419f63c11b
wmxpy <wm@mengw.io>
2022-05-13 21:27:13 -0500

    add unit tests

--- test/unit/lambda.test.ts
+++ test/unit/lambda.test.ts
@@ -128,6 +128,58 @@ describe('Given {Verifier} Class', (): void => {
         });
     });
 
+    it('should be able to verify query - happy path', async (): Promise<void> => {
+
+        const headerKey: string = chance.string();
+        const headerValue: string = chance.string();
+
+        const verifier: LambdaVerifier = LambdaVerifier.create();
+        verifier.setQueryPattern(createStrictMapPattern({
+            [headerKey]: createStringPattern(),
+        }));
+
+        const wrapped: APIGatewayProxyHandler = verifier.warpAPIGateWayProxyHandler(() => {
+            return createLambdaResponse(HTTP_RESPONSE_CODE.OK);
+        });
+
+        const result: APIGatewayProxyResult = await wrapped({
+            queryStringParameters: {
+                [headerKey]: headerValue,
+            },
+        } as any, null as any, null as any) as APIGatewayProxyResult;
+
+        expect(result).to.be.deep.equal({
+            statusCode: HTTP_RESPONSE_CODE.OK,
+            body: JSON.stringify({}),
+        });
+    });
+
+    it('should be able to verify query - sad path', async (): Promise<void> => {
+
+        const headerKey: string = chance.string();
+        const headerValue: number = chance.natural();
+
+        const verifier: LambdaVerifier = LambdaVerifier.create();
+        verifier.setQueryPattern(createStrictMapPattern({
+            [headerKey]: createStringPattern(),
+        }));
+
+        const wrapped: APIGatewayProxyHandler = verifier.warpAPIGateWayProxyHandler(() => {
+            return createLambdaResponse(HTTP_RESPONSE_CODE.OK);
+        });
+
+        const result: APIGatewayProxyResult = await wrapped({
+            queryStringParameters: {
+                [headerKey]: headerValue,
+            },
+        } as any, null as any, null as any) as APIGatewayProxyResult;
+
+        expect(result).to.be.deep.equal({
+            statusCode: HTTP_RESPONSE_CODE.BAD_REQUEST,
+            body: JSON.stringify(`[Sudoo-Lambda-Verify] Invalid Query, Invalid Type of [${headerKey}]; Should be type of "string"; But got type of "number"`),
+        });
+    });
+
     it('should be able to verify body - happy path', async (): Promise<void> => {
 
         const headerKey: string = chance.string();
